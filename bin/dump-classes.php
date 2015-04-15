67b8e801d1815e7e43a9d7d869072920082718d5
Mateusz Tymek <mtymek@gmail.com>
2015-04-15 22:07:46 +0200

    fix path

--- bin/dump-classes.php
+++ bin/dump-classes.php
@@ -1,7 +1,7 @@
 #!/usr/bin/env php
 <?php
 
-(@include_once __DIR__ . '/../vendor/autoload.php') || @include_once __DIR__ . '/../autoload.php';
+(@include_once __DIR__ . '/../vendor/autoload.php') || @include_once __DIR__ . '/../../../autoload.php';
 
 $app = new \ClassDumper\DumperApp();
 $app->run();
